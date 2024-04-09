4b12752b59d98bb5e53e79a69003d4484b502820
Joël Ammann <joel@simplywing.ch>
2024-04-09 21:32:14 +0200

    update JavaFX

--- joam/src/main/java/ch/hslu/ad/helper/FXCanvas2.java
+++ joam/src/main/java/ch/hslu/ad/helper/FXCanvas2.java
@@ -28,6 +28,7 @@ public class FXCanvas2 extends Application {
     // Set your panel size here
     private static final int FRAME_WIDTH = 1280;
     private static final int FRAME_HEIGHT = 800;
+    private static final String WINDOW_TITLE = "JavaFX Canvas";
 
     public static void main(String[] args) {
         launch(args);
@@ -45,6 +46,7 @@ public class FXCanvas2 extends Application {
         stage.setWidth(FRAME_WIDTH);
         stage.setHeight(FRAME_HEIGHT);
         stage.getIcons().add(new Image(Objects.requireNonNull(FXCanvas.class.getResourceAsStream("/icon2.png"))));
+        stage.setTitle(WINDOW_TITLE);
 
         // Get screen refresh rate and apply it to animation
         stage.addEventHandler(WindowEvent.WINDOW_SHOWN, e -> {
@@ -68,17 +70,17 @@ public class FXCanvas2 extends Application {
 class SmootherAnimation extends Canvas {
 
     private static final double SPEED = 250; // Speed value to be applied in either direction in px/s
-    private static final Point2D RECT_DIMS = new Point2D(50, 50); // rectangle size
+    private static final Point2D BALL_DIMS = new Point2D(50, 50);
     // Canvas painting colors
     private static final Paint BLACK = Paint.valueOf("black");
     private static final Paint RED = Paint.valueOf("red");
     private static final Paint GREEN = Paint.valueOf("ForestGreen");
     private static final Paint BLUE = Paint.valueOf("SteelBlue");
     long[] frameTimes = new long[120]; //length defines number of rendered frames to average over
-    // Defines rectangle start position, stores current position
-    private Point2D recPos = new Point2D(0, 50);
     // Defines initial speed, stores current speed
     private Point2D speed = new Point2D(SPEED, SPEED);
+    // Defines rectangle start position, stores current position
+    private Point2D recPos = new Point2D(0, 50);
     //Frame rate measurement
     private long frameCount = 0;
     private BigDecimal fps = null;
@@ -150,11 +152,17 @@ class SmootherAnimation extends Canvas {
     private void updateWorld(long previousFrame, long nextFrame) {
         double elapsed = (nextFrame - previousFrame) / 1e9; //Interval in seconds
         // Reverse when hitting borders
-        if (rectHitsBorders(recPos.getX(), recPos.getY(),
-                RECT_DIMS.getX(), RECT_DIMS.getY(),
-                speed.getX(), speed.getY())) {
-            speed = speed.multiply(-1.);
+        BorderHit hit = rectHitsBorders(recPos.getX(), recPos.getY(),
+                BALL_DIMS.getX(), BALL_DIMS.getY(),
+                speed.getX(), speed.getY());
+
+        switch (hit) {
+            case TOP, BOTTOM -> speed = new Point2D(speed.getX(), speed.getY() * -1);
+            case LEFT, RIGHT -> speed = new Point2D(speed.getX() * -1, speed.getY());
+            case NO_HIT -> {
+            }
         }
+
         // Update position according to speed
         recPos = recPos.add(speed.multiply(elapsed));
     }
@@ -171,7 +179,7 @@ class SmootherAnimation extends Canvas {
         gfx.strokeRect(0, 0, getWidth(), getHeight());
         // Draw moving shape
         gfx.setFill(RED);
-        gfx.fillRect(recPos.getX(), recPos.getY(), RECT_DIMS.getX(), RECT_DIMS.getY());
+        gfx.fillOval(recPos.getX(), recPos.getY(), BALL_DIMS.getX(), BALL_DIMS.getY());
         // Draw FPS meter
         String fpsText = fps == null ? "FPS" : fps.toString();
         gfx.setTextAlign(TextAlignment.RIGHT);
@@ -194,20 +202,24 @@ class SmootherAnimation extends Canvas {
      * @param height considered rectangle height
      * @param speedX speed component in x direction
      * @param speedY speed component in y direction
-     * @return true if a canvas border is crossed in the direction of movement
+     * @return BorderHit with the information where and if the border was hit
      */
-    private boolean rectHitsBorders(double x, double y, double width, double height, double speedX, double speedY) {
+    private BorderHit rectHitsBorders(double x, double y, double width, double height, double speedX, double speedY) {
         Rectangle2D frame = new Rectangle2D(0, 0, getWidth(), getHeight());
         Rectangle2D rect = new Rectangle2D(x, y, width, height);
         if (speedX < 0 && rect.getMinX() < frame.getMinX())
-            return true;
+            return BorderHit.LEFT;
         else if (speedX > 0 && rect.getMaxX() > frame.getMaxX())
-            return true;
+            return BorderHit.RIGHT;
         else if (speedY < 0 && rect.getMinY() < frame.getMinY())
-            return true;
+            return BorderHit.TOP;
         else if (speedY > 0 && rect.getMaxY() > frame.getMaxY())
-            return true;
-        return false;
+            return BorderHit.BOTTOM;
+        return BorderHit.NO_HIT;
+    }
+
+    private enum BorderHit {
+        TOP, BOTTOM, LEFT, RIGHT, NO_HIT
     }
 
 }
